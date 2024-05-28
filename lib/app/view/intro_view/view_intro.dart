f012203db2fe30041e5f5b20854a4fea6c36db6d
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-05-29 00:31:21 +0300

    add ✨:into page

--- lib/app/view/intro_view/view_intro.dart
+++ lib/app/view/intro_view/view_intro.dart
@@ -9,10 +9,41 @@ class IntroView extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
+          SizedBox(height: MediaQuery.of(context).size.height * 0.08),
           Padding(
-            padding: const EdgeInsets.all(0),
+            padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.07),
             child: Image.asset(Assets.images.png.avocado.path),
-          )
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.symmetric(
+                horizontal: MediaQuery.of(context).size.height * 0.05),
+            child: const Text(
+              "We Deliver Groceries At Your Doorstep",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+          const Text(
+            "Fresh items everyday",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 20),
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+          //get started button
+          Container(
+              decoration: BoxDecoration(
+                  color: Colors.green, borderRadius: BorderRadius.circular(15)),
+              padding:
+                  EdgeInsets.all(MediaQuery.of(context).size.height * 0.03),
+              child: const Text(
+                "Get Start",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ))
         ],
       ),
     );
