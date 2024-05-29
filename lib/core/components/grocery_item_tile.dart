63e51702cf4bbe47ffe06d8f60c69778fcab0450
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-05-30 02:35:46 +0300

    update 🆕: component

--- lib/core/components/grocery_item_tile.dart
+++ lib/core/components/grocery_item_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_shop_app/gen/assets.gen.dart';
 
 class GroceryItemTile extends StatelessWidget {
   final String itemName;
@@ -14,6 +15,35 @@ class GroceryItemTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: color.withOpacity(0.4),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child:
+            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          //image
+          Image.asset(
+            imagePath,
+            height: 70,
+          ),
+          //item name
+          Text(itemName),
+          //price button
+          MaterialButton(
+            onPressed: () {},
+            color: color,
+            child: Text(
+              '\$$itemPrice',
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
