505cc31db716ebfce26fc619f1fc7e27973d7cf3
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-05-30 02:36:19 +0300

    update 🆕: cart model

--- lib/app/model/cart_model.dart
+++ lib/app/model/cart_model.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'package:grocery_shop_app/gen/assets.gen.dart';
+
 class CartModel extends ChangeNotifier {
   //list of items on sale
-  final List _shopItems = [
-//[itemName,itemPrice,imagePath,color]
-    ["Avacado", "4.00", "Assets.images.png.avocado.path", Colors.green],
-    ["Banana", "2.50", "Assets.images.png.banana.path", Colors.yellow],
-    ["Chicken", "12.80", "Assets.images.png.chicken.path", Colors.brown],
-    ["Water", "1.00", "Assets.images.png.water.path", Colors.blue],
+  final List<List<dynamic>> _shopItems = [
+    //[itemName, itemPrice, imagePath, color]
+    ["Avocado", "4.00", Assets.images.png.avocado.path, Colors.green],
+    ["Banana", "2.50", Assets.images.png.banana.path, Colors.orange],
+    ["Chicken", "12.80", Assets.images.png.chicken.path, Colors.brown],
+    ["Water", "1.00", Assets.images.png.water.path, Colors.blue],
   ];
 
-  get shopItems => _shopItems;
+  List<List<dynamic>> get shopItems => _shopItems;
 }
